e078e991fd32b13e4964b1bc222a29c5c72206f3
hanzeloliver <hanzel.oliver55@gmail.com>
2025-09-15 11:10:05 +0700

    Commit No. 3 – use dynamic fragment

--- app/src/main/java/com/example/lab_week_03/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_03/MainActivity.kt
@@ -5,26 +5,46 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
-interface CoffeeListener {
-    fun onSelected(id: Int)
-}
+import androidx.fragment.app.FragmentContainerView
 
 class MainActivity : AppCompatActivity(), CoffeeListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        // Handle window insets (boleh dipertahankan, ini bawaan template)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.fragment_container)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Tambahkan ListFragment pertama kali
+        if (savedInstanceState == null) {
+            findViewById<FragmentContainerView>(R.id.fragment_container).let { containerLayout ->
+                val listFragment = ListFragment()
+                supportFragmentManager.beginTransaction()
+                    .add(containerLayout.id, listFragment)
+                    .commit()
+            }
+        }
     }
 
     override fun onSelected(id: Int) {
-        val detailFragment = supportFragmentManager
-            .findFragmentById(R.id.fragment_detail) as DetailFragment
+        // Buat DetailFragment baru
+        val detailFragment = DetailFragment()
+
+        findViewById<FragmentContainerView>(R.id.fragment_container).let { containerLayout ->
+            supportFragmentManager.beginTransaction()
+                .replace(containerLayout.id, detailFragment)
+                .addToBackStack(null) // supaya bisa back ke ListFragment
+                .commit()
+        }
+
+        // Tunggu transaksi selesai, lalu update data
+        supportFragmentManager.executePendingTransactions()
         detailFragment.setCoffeeData(id)
     }
 }
